810993c0cf8170918476b4b7cab4d8ed699f1cc9
pranav <pranavsworkemail@gmail.com>
2023-11-26 02:02:57 -0500

    made about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { About } from './components/About';
 import { Main } from './components/Main';
 import { Message } from './components/Message';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className='App'>
       <Main />
+      <About />
       <Message />
     </div>
   );
---
+++ frontend/src/components/About.js
@@ -0,0 +1,13 @@
+import { Container, Row , Col } from "react-bootstrap";
+import { useState, useEffect } from "react";
+
+export const About = () => {
+
+
+    return(
+        <div>
+            <p>hello</p>
+        </div>
+    )
+
+}
